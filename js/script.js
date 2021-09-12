83012d87238548d49b26f809bb9f9211a1e7e4e9
Aruneshwaran <65992799+Arun3sh@users.noreply.github.com>
2021-09-12 12:10:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ async function getData() {
 		var img = document.getElementById('res');
 		img.src = data.results[0].picture.large;
 		var content = `<p>Name : ${data.results[0].name.title}.${data.results[0].name.first} ${data.results[0].name.last}</p>
-		<br> <p>email Id: ${data.results[0].email}</p> <br> <p>email Id: ${data.results[0].dob.date}</p> `;
+		<br> <p>email Id: ${data.results[0].email}</p> <br> <p>DOB: ${data.results[0].dob.date}</p> `;
 		//var name = console.log(data.results[0].name.first);
 		var getContent = document.querySelector('.content');
 		getContent.innerHTML = content;
